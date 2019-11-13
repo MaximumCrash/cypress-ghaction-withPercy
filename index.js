476a11d8205c211e793008442b4e019faeac3977
Réjon Taylor-Foster <rejon@maximumcrash.com>
2019-11-13 13:26:49 -0500

    Update index.js

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ const installPercy = () => {
 
 const buildProject = () => {
   console.log('Running: next build && next export')
-  return exec.exec('npm run build')
+  return exec.exec('npx next build && npx next export')
 }
 
 const startServerAndTest = () => {
