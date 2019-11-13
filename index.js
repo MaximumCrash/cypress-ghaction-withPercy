06b1490788de2ca90c7fafe2b4a5cb3dd16208b2
Réjon Taylor-Foster <rejon@maximumcrash.com>
2019-11-13 11:46:00 -0500

    Update index.js

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ const installPercy = () => {
     return
   }
   
-  return exec.exec('npx install --save-dev @percy/cypress')
+  return exec.exec('npm install --save-dev @percy/cypress')
 }
 
 const install = () => {
